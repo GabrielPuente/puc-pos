b51fadd29e45857900fb879899c616a1aa4c1240
Gabriel Puente <gapuente96@gmail.com>
2022-02-03 20:29:21 -0300

    Consumindo metodos services reference

---
+++ AWS/Soap/Soap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Soap
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            ServiceReference1.SOAPDemoSoapClient client = new();
+            
+            var responseInterger = await client.AddIntegerAsync(50, 55);
+            Console.WriteLine($"AddIntegerAsync {responseInterger}");
+
+            Console.WriteLine();
+            
+            var responseDivide = await client.DivideIntegerAsync(10, 5);
+            Console.WriteLine($"DivideIntegerAsync {responseDivide}");
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var responsePerson = await client.FindPersonAsync("2");
+
+            Console.WriteLine("InfoPerson");
+            Console.WriteLine(responsePerson.Name);
+            Console.WriteLine(responsePerson.Age);
+            Console.WriteLine(responsePerson.Home.Street);
+            Console.WriteLine(responsePerson.Office.Street);
+        }
+    }
+}
